bd8be069e2adad2f0e7e5cd7ba9e4f5ad91392fd
rowter4 <nadzir.4@gmail.com>
2022-05-07 20:41:41 +0800

    initial commit

---
+++ src/main/java/finalProject/ssfpaf/project/SsfPafProjectApplication.java
@@ -0,0 +1,13 @@
+package finalProject.ssfpaf.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfPafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfPafProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finalProject/ssfpaf/project/SsfPafProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package finalProject.ssfpaf.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfPafProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
